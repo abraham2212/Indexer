84f6ed1619eeac7c76b3e74f1cdc6bb5e0bcdc1a
abraham2212 <ibrahimsak@code.edu.az>
2023-01-10 20:50:24 +0400

    index

---
+++ Indexer/Indexer/Indexer/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Indexer.Models;
+
+Book book1 = new Book() { Id = 1, Name = "XOSROV" };
+Book book2 = new Book() { Id = 2, Name = " 7 GOZEL" };
+
+Library library=new Library();
+library[0] = book1;
+library[2] = book2;
+
+Console.WriteLine(library[0].Name);
+Console.WriteLine(library[1].Name);
